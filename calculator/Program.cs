7535297f0dcf9d397a25e9a8169fbd6ad38b0c81
ValeryShvets <135962778+ValeryShvets@users.noreply.github.com>
2023-06-08 20:49:24 +0300

    create

---
+++ calculator/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                float firstOperant = 0; float secondOperant = 0;
+
+
+                Console.WriteLine("Input firstOperant");
+                firstOperant = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Input secondOperant");
+                secondOperant = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Choose your operation");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+
+                {
+
+                    switch (Console.ReadLine())
+                    {
+                        case "a":
+                            Console.WriteLine($"Your resutl: {firstOperant} + {secondOperant} = " + (firstOperant + secondOperant));
+                            break;
+                        case "s":
+                            Console.WriteLine($"Your resutl: {firstOperant} - {secondOperant} = " + (firstOperant - secondOperant));
+                            break;
+                        case "m":
+                            Console.WriteLine($"Your resutl: {firstOperant} * {secondOperant} = " + (firstOperant * secondOperant));
+                            break;
+                        case "d":
+                            Console.WriteLine($"Your resutl: {firstOperant} / {secondOperant} = " + (firstOperant / secondOperant));
+                            break;
+                    }
+
+                }
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+    
+    }
+    
+}
